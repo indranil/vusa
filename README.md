979ce5235de19d6cff19585fbc44171aba660faf
Indranil Dasgupta <indranild@gmail.com>
2017-10-29 06:21:33 +0530

    Added a few responses and the open command. fun stuff!

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 VUSA -- the Very Unreliable Speech Assistant
 ====================================
 
-Voice recognition now works. Assistant part being put in. Have a play around!
-
 Just a tiny little script having fun with the [WebSpeech API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API).
 
 Commands & Keywords you can use or try:
 
-* Weather
-* Hello
-* vusa
-* open facebook/twitter/google/github
+* Open 'site'
+* hello
+* how are you
+* who are you
+* where are you from
+* secret
+
+Have a play around while I put a few APIS (weather, news, trending tweets) in.
 
 More to come... I'm just playing around to be honest. Fork/contribute/have fun! 😊
--- main.js
+++ main.js
@@ -5,32 +5,51 @@ var speaking = false;
 let speakButton = document.querySelector('.mic');
 let speechArea = document.querySelector('.speech-area');
 
+// Dictionaries & URLs & APIs
+const responsesDict = {
+  "hello": "Hi there!",
+  "hi": "Hi there",
+  "how are you": "I am fine! How about you?",
+  "where are you from": "You know what, I'm not really sure! Github? Is that a place?",
+  "who are you": "I'm VUSA, the Very Unique Speech Advisor!",
+  "secret": "Shh! It's a secret!",
+};
+
+const vusaOpens = {
+  'facebook': 'http://www.facebook.com',
+  'github': 'http://github.com',
+  'twitter': 'http://twitter.com',
+  'google': 'http://google.com',
+  'apple': 'http://apple.com',
+  'bbc': 'http://bbc.com',
+  'cnn': 'http://cnn.com',
+  'yahoo': 'http://yahoo.com',
+  'wikipedia': 'http://wikipedia.org',
+  'youtube': 'http://youtube.com',
+  'reddit': 'http://reddit.com',
+  'netflix': 'http://netflix.com',
+}
+
 // Because chrome only accepts the webkit prefixed functions, just make everything the same
 // by putting it to the same var
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
-
-
 const recognition = new SpeechRecognition();
-const grammarList = new SpeechGrammarList();
-
-// create the grammar list?
 
 // some rules for our vusa
 recognition.interimResults = true;
 recognition.lang = 'en-US';
-// recognition.grammars = grammarList;
+
 
 
 // Some functions needed
-let handleSpeechEnd = () => {
+const handleSpeechEnd = () => {
   let currentUsah = document.querySelector('p.usah.current');
   recognition.stop();
   
   // clean the last said bubble if they didn't say anything
-  if (currentUsah.textContent === '• • •') {
+  if (currentUsah !== null && currentUsah.textContent === '• • •') {
     currentUsah.remove();
   }
   
@@ -39,17 +58,58 @@ let handleSpeechEnd = () => {
   speakButton.classList.remove('on');
 };
 
+// sets up the vusa bubble with the response
+const vusaResponse = (response) => {
+  let vusa = document.createElement('p');
+  vusa.classList.add('vusa');
+  vusa.textContent = response;
+  
+  speechArea.appendChild(vusa);
+};
+
+const vusaAssist = (speech) => {
+  speech = speech.toLowerCase();
+  
+  // Normal responses
+  for (let key in responsesDict) {
+    if (speech.includes(key)) {
+      vusaResponse(responsesDict[key]);
+    }
+  }
+  
+  // Open command
+  let openCmd = /open (\w*)/i;
+  if (speech.match(openCmd)) {
+    let cmd = openCmd.exec(speech);
+    
+    if (vusaOpens.hasOwnProperty(cmd[1])) {
+      // this is just for fun! :P
+      let lazyResponses = [
+        "Can't you type, o lazy one?",
+        "Wow you are lazy!",
+        "One minute with me, and you've already forsaken the keyboard?",
+        "What will you do without me, hmm?",
+        "Why yes of course!",
+      ];
+      window.open(vusaOpens[cmd[1]]);
+      vusaResponse(lazyResponses[Math.floor(Math.random() * lazyResponses.length)]);
+    } else {
+      vusaResponse("I'm sorry, but I don't know what that is!");
+    }
+  }
+}
+
 
 speakButton.addEventListener('click', function(e) {
   e.preventDefault();
   speaking = !speaking;
   this.classList.toggle('on');
   
-  // set up the thingy
+  // set up the user bubble
   if (speaking) {
     let usah = document.createElement('p');
     usah.classList.add('usah', 'current');
-    usah.textContent = '• • •';
+    usah.textContent = "• • •";
     speechArea.appendChild(usah);
     
     // let's recognise some voices!
@@ -81,5 +141,6 @@ recognition.onresult = function(e) {
     currentUsah.classList.remove('current');
     // now we have the final speech
     // we can go through the apis and actually assist...
+    vusaAssist(speech);
   }
 }
