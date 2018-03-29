5f0f4d4b5986a188d3a3062279d673a5e2ecc22f
Indranil Dasgupta <indranild@gmail.com>
2018-03-30 00:50:09 +0200

    Extracted dictionaries to their own file

---
+++ src/dict.js
@@ -0,0 +1,28 @@
+// Dictionaries & URLs & APIs
+const responsesDict = {
+  "hello": "Hi there!",
+  "hi": "Hi there",
+  "hey": "Hi there! How are you doing this fine day?",
+  "how are you": "I am fine! How about you?",
+  "where are you from": "You know what, I'm not really sure! Github? Is that a place?",
+  "who are you": "I'm VUSA, the Very Unique Speech Advisor!",
+  "secret": "Shh! It's a secret!",
+  "thank you": "you're welcome!",
+};
+
+const vusaOpens = {
+  'facebook': 'http://www.facebook.com',
+  'github': 'http://github.com',
+  'twitter': 'http://twitter.com',
+  'google': 'http://google.com',
+  'apple': 'http://apple.com',
+  'bbc': 'http://bbc.com',
+  'cnn': 'http://cnn.com',
+  'yahoo': 'http://yahoo.com',
+  'wikipedia': 'http://wikipedia.org',
+  'youtube': 'http://youtube.com',
+  'reddit': 'http://reddit.com',
+  'netflix': 'http://netflix.com',
+}
+
+export { responsesDict, vusaOpens };
--- src/main.js
+++ src/main.js
@@ -2,38 +2,12 @@
 // - Very Unreliable Speech Assistant
 
 import axios from 'axios';
+import { responsesDict, vusaOpens } from './dict';
 
 // here we go!
 let speaking = false;
 const speakButton = document.querySelector('.mic');
 const speechArea = document.querySelector('.speech-area');
-
-// Dictionaries & URLs & APIs
-const responsesDict = {
-  "hello": "Hi there!",
-  "hi": "Hi there",
-  "hey": "Hi there! How are you doing this fine day?",
-  "how are you": "I am fine! How about you?",
-  "where are you from": "You know what, I'm not really sure! Github? Is that a place?",
-  "who are you": "I'm VUSA, the Very Unique Speech Advisor!",
-  "secret": "Shh! It's a secret!",
-  "thank you": "you're welcome!",
-};
-
-const vusaOpens = {
-  'facebook': 'http://www.facebook.com',
-  'github': 'http://github.com',
-  'twitter': 'http://twitter.com',
-  'google': 'http://google.com',
-  'apple': 'http://apple.com',
-  'bbc': 'http://bbc.com',
-  'cnn': 'http://cnn.com',
-  'yahoo': 'http://yahoo.com',
-  'wikipedia': 'http://wikipedia.org',
-  'youtube': 'http://youtube.com',
-  'reddit': 'http://reddit.com',
-  'netflix': 'http://netflix.com',
-}
 
 // Because chrome only accepts the webkit prefixed functions, just make everything the same
 // by putting it to the same var
